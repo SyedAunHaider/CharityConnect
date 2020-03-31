acb602114ca61cfb0607494739233e9d32094938
Danial Shaukat <Danial.shaukat@synergy-it.com>
2020-03-31 13:37:27 +0500

    Structure changes, add registration page

---
+++ CharityConnect/Backend/DataAccess/RegistrationModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CharityConnect.Backend.DataAccess
+{
+    public class RegistrationModel
+    {
+    }
+}
---
+++ CharityConnect/ClientApp/src/app/user-registration/user-registration.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './user-registration.component.html',
+})
+export class UserRegistrationComponent {
+
+}
